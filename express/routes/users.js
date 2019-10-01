5651c9e3108733391f1ffb53c8dd45261c4fcaa2
hi-oda <hi-oda@global-mobility-service.com>
2019-10-01 11:41:53 +0900

    add api

--- express/routes/users.js
+++ express/routes/users.js
@@ -7,4 +7,24 @@ router.get('/', async (req, res, next) => {
   res.json(rows);
 });
 
+router.get('/:userId', async (req, res, next) => {
+  const rows = await queryAsync('select * from m_user where id = ?', req.params.userId).catch(err => {res.json(err)});
+  res.json(rows[0]);
+});
+
+router.post('/', async (req, res, next) => {
+  const result = await queryAsync('insert into m_user (name) values (?)', req.body.name).catch(err => {res.json(err)});
+  res.json({id: result.insertId});
+});
+
+router.put('/:userId', async (req, res, next) => {
+  await queryAsync('update m_user set name = ? where id = ?', [req.body.name, req.params.userId]).catch(err => {res.json(err)});
+  res.json({});
+});
+
+router.delete('/:userId', async (req, res, next) => {
+  await queryAsync('delete from m_user where id = ?', req.params.userId).catch(err => {res.json(err)});
+  res.json({});
+});
+
 module.exports = router;
